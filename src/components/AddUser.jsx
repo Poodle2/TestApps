0ea74e0abd0d5bcbe4c310e882f44b820814bcb0
sviatoslav <SvyatoslavPud@gmail.com>
2023-03-19 20:50:22 +0200

    AddUser was formatted

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -7,9 +7,9 @@ const AddUser = (props) => {
 	return (
 		
 		<form>
-			<input placeholder='FirstName' onChange={event => setData(prev => ({...prev ,firstName: event.target.value}))} value={data.firstName}/>
-			<input placeholder='LastName' onChange={event => setData(prev => ({...prev ,firstName: event.target.value}))} value={data.lastName}/>
-			<input placeholder='Age' onChange={event => setData(prev => ({...prev ,firstName: event.target.value}))} value={data.age}/>
+			<input placeholder='FirstName' onChange={event => setData(prev => ({...prev, firstName: event.target.value}))} value={data.firstName}/>
+			<input placeholder='LastName' onChange={event => setData(prev => ({...prev, lastName: event.target.value}))} value={data.lastName}/>
+			<input placeholder='Age' onChange={event => setData(prev => ({...prev, age: event.target.value}))} value={data.age}/>
 			<button type='button' onClick={() => {
 				props.onAdd({data});
 			}
